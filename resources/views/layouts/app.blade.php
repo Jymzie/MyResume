8343f7d7f5e378676d39365cf8fcd9a4b9d66703
Jimwell Punzalan <punzalanjimwell@gmail.com>
2024-07-30 18:55:57 +0800

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MyResume</title>
+    <title>JymzResume</title>
 </head>
 <body>
     <div id="app"></div>
